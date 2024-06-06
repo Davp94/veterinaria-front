8482b3614559c7e4050f4fc228ca5fe249c11cb4
david_villca <david_villca@contraloria.gob.bo>
2024-06-06 16:50:41 -0400

    sorting filtering and pagination

--- src/feature/mascotas/components/mascotas.main.jsx
+++ src/feature/mascotas/components/mascotas.main.jsx
@@ -5,32 +5,40 @@ import { classNames } from 'primereact/utils';
 import { useMascotas } from '../hooks/mascotas.hook';
 import { findAllMascotasPaginacion } from '../services/mascotas.service';
 import { Dropdown } from 'primereact/dropdown';
+import { InputText } from 'primereact/inputtext';
+import Image from 'next/image';
 export default function MascotasMain() {
   const { mascotas, loadingMascotas, fetchFindAllMascotas } = useMascotas();
+  const [valueFilter, setValueFilter] = useState('');
+  const [valueAscDesc, setValueAscDesc] = useState(null);
   const [lazyState, setlazyState] = useState({
     first: 0,
-    rows: 5,
+    rows: 2,
     page: 0,
     sortField: 'nombre',
     sortOrder: 1,
   });
 
   const onPage = event => {
+    event.sortOrder = valueAscDesc ? valueAscDesc.order: 1;
     setlazyState(event);
   };
 
-  const sortOptions = [
-    { label: 'Price High to Low', value: '!price' },
-    { label: 'Price Low to High', value: 'price' }
-];
+  const sortFields = [
+    { name: 'Ascendente', order: 1 },
+    { name: 'Descendente', order: 0 }
+  ];
 
-  const getLazyData = () => {
+  const getLazyData = (value) => {
     const bodyFiltering = {};
     (bodyFiltering.size = lazyState.rows),
       (bodyFiltering.page = lazyState.page || 0),
       (bodyFiltering.sortParam = lazyState.sortField || 'nombre'),
-      (bodyFiltering.order = lazyState.sortOrder || 1);
-    fetchFindAllMascotas(bodyFiltering);
+      (bodyFiltering.order = lazyState.sortOrder);
+      if(value){
+        bodyFiltering.nombre = value;
+      }
+      fetchFindAllMascotas(bodyFiltering);
   };
 
   useEffect(() => {
@@ -44,11 +52,13 @@ export default function MascotasMain() {
         key={mascota.id}
       >
         <div className={classNames('flex flex-column xl:flex-row xl:align-items-start p-4 gap-4', { 'border-top-1 surface-border': index !== 0 })}>
-          {/* <img
+          <img
             className='w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round'
-            src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`}
+            src={`http://localhost:8200/images/${mascota.imagen}`}
             alt={mascota.nombre}
-          /> */}
+            width={200}
+            height={150}
+          />
           <div className='flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4'>
             <div className='flex flex-column align-items-center sm:align-items-start gap-3'>
               <div className='text-2xl font-bold text-900'>{mascota.nombre}</div>
@@ -75,9 +85,41 @@ export default function MascotasMain() {
     return <div className='grid grid-nogutter'>{list}</div>;
   };
 
+  const onSortChange = value => {
+    setValueAscDesc(value);
+    setlazyState((prevState)=>({...prevState, sortOrder: value.order}));
+  };
+
+  const filterData = () => {
+    getLazyData(valueFilter);
+  }
+
+
   const header = () => {
-    return <Dropdown options={sortOptions} value={sortKey} optionLabel="label" placeholder="Sort By Price" onChange={onSortChange} className="w-full sm:w-14rem" />;
-};
+    return (
+      <div className='flex justify-content-between'>
+        <Dropdown
+          value={valueAscDesc}
+          onChange={(e) => onSortChange(e.value)}
+          options={sortFields}
+          optionLabel='name'
+          placeholder='Orden by Nombre'
+          className='w-full sm:w-14rem'
+        />
+        <div>
+          <InputText
+            value={valueFilter}
+            onChange={e => setValueFilter(e.target.value)}
+            placeholder='filtro de busqueda por nombre'
+          />
+          <Button
+            icon='pi pi-search'
+            onClick={() => filterData()}
+          />
+        </div>
+      </div>
+    );
+  };
 
   return (
     <>
@@ -86,9 +128,9 @@ export default function MascotasMain() {
         title='Listado de Mascotas'
       >
           <DataView
-            value={mascotas.content ? mascotas.content : []}
+            value={mascotas?.content ? mascotas.content : []}
             lazy
-            totalRecords={mascotas.totalElements ? mascotas.totalElements : 0}
+            totalRecords={mascotas?.totalElements ? mascotas.totalElements : 0}
             first={lazyState?.first}
             loading={loadingMascotas}
             onPage={onPage}
